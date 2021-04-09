2123c70ba336cb0365231eb894f301caefffdd10
Nianyi Wang <WangNianyi2001@outlook.com>
2021-04-09 15:02:38 +0800

    Create abstract interface class Game

--- game/game.h
+++ game/game.h
@@ -1,4 +1,31 @@
 #pragma once
 
-constexpr int width = 384, height = 288;
-int scale = 2;
+#include "SimpleWin32Lib.h"
+
+namespace Game {
+	using namespace SimpleWin32;
+
+	constexpr int virtual_width = 384, virtual_height = 288;
+	int scale = 2;
+
+	EventHandler event_handler;
+	LRESULT CALLBACK eventProcessor(HWND hWnd, UINT type, WPARAM wParam, LPARAM lParam) {
+		return event_handler(hWnd, type, wParam, lParam);
+	}
+
+	Window *window;
+
+	void initGame(HINSTANCE hInstance) {
+		window = new Window(hInstance, Window::InitArg{
+			.title = L"Crazy Bicycle",
+			.width = virtual_width * scale,
+			.height = virtual_height * scale,
+			.event_processor = eventProcessor,
+		});
+		event_handler.addHandler(WM_DESTROY, EventHandler::defaultDestroyHandler);
+	}
+
+	int runGame() {
+		return window->run();
+	}
+}
--- game/main.cpp
+++ game/main.cpp
@@ -1,25 +1,11 @@
-#include "SimpleWin32Lib.h"
 #include "game.h"
 
-using namespace SimpleWin32;
-
-EventHandler event_handler;
-LRESULT CALLBACK eventProcessor(HWND hWnd, UINT type, WPARAM wParam, LPARAM lParam) {
-	return event_handler(hWnd, type, wParam, lParam);
-}
+using namespace Game;
 
 int APIENTRY wWinMain(
 	HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPWSTR lpCmdLine, int nCmdShow
 ) {
-	Window window(hInstance, Window::InitArg{
-		.title = L"Crazy Bicycle",
-		.width = width * scale,
-		.height = height * scale,
-		.event_processor = &eventProcessor,
-	});
-
-	event_handler.addHandler(WM_DESTROY, &EventHandler::defaultDestroyHandler);
-
-	return window.run();
+	initGame(hInstance);
+	return runGame();
 }
